76e53e077ac4f33467cc07cf73fc2cb22fda0af4
omar0azx <a3mr.azx@gmail.com>
2021-10-02 16:46:57 +0300

    Initial Commit

---
+++ الاذكار/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  الاذكار
+//
+//  Created by omar alzhrani on 25/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
